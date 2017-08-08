ada0593046c892e7006cd3bbedb1f17f944840c3
MB DR <mderoberts@users.noreply.github.com>
2017-08-08 00:28:48 -0400

    js setup, database and table set up

---
+++ bamazon.sql
@@ -0,0 +1,17 @@
+-- CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+-- CREATE TABLE products (
+--  item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+--  product_name VARCHAR(50) NOT NULL,
+--  department_name VARCHAR(50) NOT NULL,
+--  price INTEGER(11) NOT NULL,
+--  stock_quantity INTEGER(11) NOT NULL,
+--  PRIMARY KEY (item_id)
+-- )
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES(
+"How to be an Awesome JavaScript Developer", "Books", $24.99, 32
+)
+
