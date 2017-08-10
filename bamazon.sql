4ae486df0caa9316e485ad9eeb0863905b38f1eb
MB DR <mderoberts@users.noreply.github.com>
2017-08-10 00:37:17 -0400

    table set

--- bamazon.sql
+++ bamazon.sql
@@ -11,7 +11,19 @@ USE bamazonDB;
 --  PRIMARY KEY (item_id)
 -- )
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES(
-"How to be an Awesome JavaScript Developer", "Books", $24.99, 32
-)
+-- ALTER TABLE products 
+-- ADD prime_subscriptions INTEGER(11) AFTER stock_quantity;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Bamazon Echo", "Electronics", 180, 32),
+  ("Bamazon Kindle", "Electronics", 80, 100),
+  ("How to be an Awesome JavaScript Developer", "Books", 25, 32),
+  ("Star Wars: Episodes IV-VI", "Movies & TV", 60, 15),
+  ("Garden Gnome", "Home & Garden", 35, 12),
+  ("Bananas", "Food & Grocery", 4, 22),
+  ("Creedance Clearwater Revival box set", "Music", 80, 30),
+  ("Basketball", "Recreation", 12, 15),
+  ("Motor oil", "Automotive", 5, 26),
+  ("Parks and Recreation DVD series", "Movies & TV", 50, 14);
+
 
