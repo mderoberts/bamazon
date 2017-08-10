34ca99821f3aaafbf68bdad57ff55cf4ca20f620
MB DR <mderoberts@users.noreply.github.com>
2017-08-10 00:57:39 -0400

    prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,3 +8,24 @@ var connection = mysql.createConnection({
     password: "",
     database: "bamazonDB"
 });
+
+connection.connect(function(err) {
+    if(err) throw err;
+    startShopping();
+});
+
+function startShopping() {
+    inquirer.prompt ({
+        name: "selectId",
+        type: "rawlist",
+        message: "What would you like to buy?"
+    } , {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to buy?"
+    }).then(function(answer) {
+        switch (answer.action) {
+            case 
+        }
+    })
+}
